734f1019fd6ba47c38f4d73e145fa905f0bbb835
thumbthing <ilscc0902@gmail.com>
2025-10-30 19:54:42 +0900

    feat(App-getPurchase, errorHandle): 구매 금액 입력 기능 적용, 에러처리
    적용 기능
    - 안내 문구 출력, 사용자 입력 반환
    - 사용자 입력값 유효성 검증
    
    에러처리
    - 해당하는 에러 화면에 출력
    - 다시 실행해야하는 기능 재실행

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { Console } from "@woowacourse/mission-utils";
+import UserInput from "./UI/UserInput.js";
+import ValidateRawString from "./validate/ValidateRawString.js";
+
 class App {
-  async run() {}
+
+  // 에러 발생 이후 callback 처리
+  async runCallbackAfterNoticeError(error, callbackName) {
+    Console.print(error.message);
+    return await this[callbackName]();
+  }
+
+  // 1. 구매금액 입력
+  async getPurchaseFromUser() {
+    try {
+      const purchase = await UserInput.getPurchase();
+      ValidateRawString.checkPurchase(purchase);
+      return purchase
+    } catch (error) {
+      return await this.runCallbackAfterNoticeError(error, "getPurchaseFromUser");
+    }
+  }
+
+  async run() {
+    const purchase = await this.getPurchaseFromUser();
+    // 2. 당첨 번호 입력
+    // 3. 보너스 번호 입력
+    // 4. 당첨 결과 처리
+  }
 }
 
 export default App;
