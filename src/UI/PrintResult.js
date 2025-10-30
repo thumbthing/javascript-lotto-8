bcafc024b6f77548c6b48ec976bb0a6cf477b0aa
thumbthing <ilscc0902@gmail.com>
2025-10-31 03:31:55 +0900

    feat(UI-PrintResult): 구매목록 터미널 출력 기능 추가
    구현 기능
    - 구매 목록의 요소를 배열 -> 문자열로 변환
    - 출력할 문자열 정보를 담은 배열 생성
    - 출력 문자열 생성
    - 생성된 문자열 출력

---
+++ src/UI/PrintResult.js
@@ -0,0 +1,10 @@
+import { Console } from "@woowacourse/mission-utils";
+
+export default class PrintResult {
+  static purchaseList(purchaseAmount, purchaseList) {
+    const purchaseStringList = purchaseList.map((number) => `[${number.join(', ')}]`);
+    const stringList = [`${purchaseAmount}개를 구매했습니다.`, ...purchaseStringList];
+    const purchaseString = `\n${stringList.join("\n")}\n`;
+    Console.print(purchaseString);
+  }
+}
