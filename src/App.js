3aa43985cb09b8b4fb11c12205aefb49aad5cfb0
thumbthing <ilscc0902@gmail.com>
2025-10-31 00:44:52 +0900

    feat(App-purchaseService): 변환값 담당 기능, 구매금액 담당 Service 기능 추가
    구현 기능
    - 입력값, 변환값 담당 기능의 부모 함수 추가
    - 변환값 담당 내부 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Console } from "@woowacourse/mission-utils";
 import UserInput from "./UI/UserInput.js";
 import ValidateRawString from "./validate/ValidateRawString.js";
+import InputParser from "./parse/InputParser.js";
+import ValidateNumber from "./validate/ValidateNumber.js";
 
 class App {
 
@@ -10,19 +12,38 @@ class App {
     return await this[callbackName]();
   }
 
-  // 1. 구매금액 입력
+  // 1 구매금액
+  async purchaseService() {
+    const userInput = await this.getPurchaseFromUser();
+    const purchaseAmount = await this.getPurchaseAmount(userInput);
+    return purchaseAmount;
+  }
+
+  // 1-1 구매금액 입력
   async getPurchaseFromUser() {
     try {
       const purchase = await UserInput.getPurchase();
       ValidateRawString.checkPurchase(purchase);
       return purchase
     } catch (error) {
-      return await this.runCallbackAfterNoticeError(error, "getPurchaseFromUser");
+      return await this.runCallbackAfterNoticeError(error, "purchaseService");
+    }
+  }
+
+  // 1-2 입력값 변환
+  async getPurchaseAmount(purchaseInput) {
+    try {
+      const purchaseNumber = InputParser.toNumber(purchaseInput);
+      new ValidateNumber().checkPurchaseAmount(purchaseNumber);
+      const purchaseAmount = InputParser.divideWithThousand(purchaseNumber);
+      return purchaseAmount;
+    } catch (error) {
+      return await this.runCallbackAfterNoticeError(error, "purchaseService");
     }
   }
 
   async run() {
-    const purchase = await this.getPurchaseFromUser();
+    const purchaseAmount = await this.purchaseService();
     // 2. 당첨 번호 입력
     // 3. 보너스 번호 입력
     // 4. 당첨 결과 처리
