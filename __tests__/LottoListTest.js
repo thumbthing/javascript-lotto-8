fa35ec1d3c571027745255ae4b012bfece6a7877
thumbthing <ilscc0902@gmail.com>
2025-10-31 03:21:32 +0900

    test(constructor-lottoList): 기능 변경사항 적용, 테스트 케이스 추가
    테스트 항목
    - 배열의 정렬 여부: 첫번째 요소의 최소값-마지막 요소의 최대값 판단

--- __tests__/LottoListTest.js
+++ __tests__/LottoListTest.js
@@ -5,18 +5,20 @@ describe("구매 목록 테스트", () => {
     {purchaseAmount: 3, expectedLength: 3},
     {purchaseAmount: 999, expectedLength: 999},
     {purchaseAmount: 1, expectedLength: 1},
-  ])("구매횟수: $purchaseAmount 와 동일한 길이($expectedLength)\n배열내 요소의 길이가 6\n요소들의 중복이 존재하지 않는 요소 생성", ({purchaseAmount, expectedLength}) => {
+  ])("구매횟수: $purchaseAmount 와 동일한 길이($expectedLength)\n배열내 요소의 길이가 6\n요소들의 중복이 존재하지 않는 요소 생성\n배열내 정렬 확인", async ({purchaseAmount, expectedLength}) => {
     // given
     const expectedSingleLottoLength = 6;
 
     // when
-    const lottoList = new LottoList(purchaseAmount);
+    const lottoList = await new LottoList(purchaseAmount).createList();
 
     // then
     expect(lottoList.length).toBe(expectedLength);
     lottoList.forEach((lotto) => {
       expect(lotto.length).toBe(expectedSingleLottoLength);
       expect(new Set(lotto).size).toBe(expectedSingleLottoLength);
+      expect(Math.min(...lotto)).toBe(lotto[0]);
+      expect(Math.max(...lotto)).toBe(lotto[5]);
     })
   });
 });
