88031a797df8a85d609ddf3cdad2b0a7714e3c2b
thumbthing <ilscc0902@gmail.com>
2025-10-31 04:55:20 +0900

    feat(validate-checkWinNumber): 당첨 번호 입력값 검증 기능 추가
    추가 기능
    - 초기패턴: 1자리 이상 2자리 이하의 숫자형
    - 초기패턴으로 최종 패턴을 생성
    - 최종 패턴으로 정규식을 생성
    - 생성된 정규식으로 boolean 값 생성
    - boolean 값 분기문으로 에러처리

--- src/validate/ValidateRawString.js
+++ src/validate/ValidateRawString.js
@@ -4,4 +4,12 @@ export default class ValidateRawString {
     const isValid = purchaseRegExp.test(purchaseInput);
     if (!isValid) throw new Error('[ERROR] 입력값이 숫자 형식이 아닙니다. 다시 입력해주세요')
   }
+
+  static checkWinNumber(winNumberInput) {
+    const pattern = '(?:\d)?(?:\d)';
+    const patternList = `^${Array(6).fill(pattern).join(',')}$`
+    const winNumberRegExp = new RegExp(patternList);
+    const isValid = winNumberRegExp.test(winNumberInput);
+    if (!isValid) throw new Error('[ERROR] 입력값이 유효하지 않습니다.');
+  }
 }
