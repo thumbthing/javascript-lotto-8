45e5df47039be51171242d2086849135b0081697
thumbthing <ilscc0902@gmail.com>
2025-10-31 07:04:21 +0900

    feat(Lotto): 기능 개선, 추가
    구현한 기능
    - 유효성: 요소 6개, 유효한 정수, 중복 여부, 1 ~ 45 사이의 값
    - getter

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,18 @@ class Lotto {
   }
 
   #validate(numbers) {
-    if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
-    }
+    if (numbers.length !== 6) throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    if (numbers.some((num) => !Number.isSafeInteger(num))) throw new Error("[ERROR] 로또 번호가 유효하지 않은 수로 이루어져 있습니다.");
+    if (numbers.length !== new Set(numbers).size) throw new Error("[ERROR] 로또 번호는 중복되지 않은 수로 이루어져야합니다.");
+    numbers.some((num) => {
+      if (num < 1 || num > 45) throw new Error("[ERROR] 로또 번호는 1 ~ 45 사이의 값이어야 합니다");
+    });
   }
 
-  // TODO: 추가 기능 구현
+  get() {
+    const lotto = this.#numbers;
+    return lotto;
+  }
 }
 
 export default Lotto;
