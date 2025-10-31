9d9ef39fcba89841af579dabb043d6bb80ff7566
thumbthing <ilscc0902@gmail.com>
2025-11-01 00:56:12 +0900

    test(LottoResult-filterMatchList): 필터링 기능 테스트 코드-항목 추가
    테스트 항목
    - 일치 횟수 3이상인 값

--- __tests__/LottoResultTest.js
+++ __tests__/LottoResultTest.js
@@ -37,4 +37,34 @@ describe("당첨 기록", () => {
       expect(result.unMatchedNumber).toEqual(expectedList[index].unMatchedNumber);
     });
   });
+
+  test("3개 이상 일치하는 당첨 목록 생성", async () => {
+    // given
+    const purchaseList = [
+      [1,2,3,4,5,6],
+      [1,2,3,4,5,7],
+      [1,2,3,4,5,8],
+      [1,2,3,4,7,8],
+      [1,2,3,7,8,9],
+      [1,2,7,8,9,10],
+      [1,7,8,9,10,11],
+      [7,8,9,10,11,12]
+    ];
+    const lotto = new Lotto([1,2,3,4,5,6]);
+    const bonusNumber = 7;
+    const lottoResult = new LottoResult(purchaseList, lotto, bonusNumber);
+    const expectedList = [
+      {"matchCount": 6,"unMatchedNumber": []},
+      {"matchCount": 5,"unMatchedNumber": [7]},
+      {"matchCount": 5,"unMatchedNumber": [8]},
+      {"matchCount": 4,"unMatchedNumber": [7,8]},
+      {"matchCount": 3,"unMatchedNumber": [7,8,9]},
+    ]
+    
+    // when
+    const matchResult = await lottoResult.getMatchList();
+    const filterdResult = await lottoResult.filterMatchList(matchResult);
+    // then
+    expect(filterdResult.length).toBe(expectedList.length);
+  });
 });
