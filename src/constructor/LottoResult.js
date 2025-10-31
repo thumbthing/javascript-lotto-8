c842e3999aad7b8d70f39acd941fe3a1a8766596
thumbthing <ilscc0902@gmail.com>
2025-11-01 00:32:58 +0900

    style(LottoResult): 메서드 기능 주석으로 설명

--- src/constructor/LottoResult.js
+++ src/constructor/LottoResult.js
@@ -6,11 +6,13 @@ export default class LottoResult {
     this.status = this.#initializeStatus();
   }
 
+  // 당첨 기록 초기화
   #initializeStatus() {
     const status = Array(5).fill([]).map((stat, index) => [index + 3, 0]);
     return new Map(status);
   }
 
+  // 구매 목록의 요소의 단일 변환
   async #getSingleMatchResult(singlePurchase) {
     const combinedSet = new Set([...this.lotto, ...singlePurchase]);
     const matchCount = 6 - (combinedSet.size - 6);
@@ -21,6 +23,7 @@ export default class LottoResult {
     return { matchCount, unMatchedNumber }
   }
 
+  // 구매 목록 => 당첨 기록 변환
   getMatchResult() {
     const purchaseResult = this.purchaseList.map((purchase) => {
       return this.#getSingleMatchResult(purchase);
@@ -28,11 +31,13 @@ export default class LottoResult {
     return purchaseResult;
   }
 
+  // 비동기로 실행할 순회 기능 처리
   async #getPromiseAllResult(callbackName) {
     const promise = await Promise.all(this[callbackName]());
     return promise;
   }
 
+  // 당첨 기록 생성
   async getPurchaseResult() {
     const purchaseResult = await this.#getPromiseAllResult("getMatchResult");
     return purchaseResult;
