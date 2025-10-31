b358e8bf3454ffa0b80563cbc5d6760ceb777ec6
thumbthing <ilscc0902@gmail.com>
2025-11-01 00:29:48 +0900

    test(LottoResult): 테스트 코드-항목 추가
    테스트 항목
    - 구매기록의 당첨 목록 생성

---
+++ __tests__/LottoResultTest.js
@@ -0,0 +1,40 @@
+import LottoResult from "../src/constructor/LottoResult";
+import Lotto from "../src/Lotto.js";
+
+describe("당첨 기록", () => {
+  test("구매목록의 일치된 기록 생성", async () => {
+    // given
+    const purchaseList = [
+      [1,2,3,4,5,6],
+      [1,2,3,4,5,7],
+      [1,2,3,4,5,8],
+      [1,2,3,4,7,8],
+      [1,2,3,7,8,9],
+      [1,2,7,8,9,10],
+      [1,7,8,9,10,11],
+      [7,8,9,10,11,12]
+    ];
+    const lotto = new Lotto([1,2,3,4,5,6]);
+    const bonusNumber = 7;
+    const lottoResult = new LottoResult(purchaseList, lotto, bonusNumber);
+    const expectedList = [
+      {"matchCount": 6,"unMatchedNumber": []},
+      {"matchCount": 5,"unMatchedNumber": [7]},
+      {"matchCount": 5,"unMatchedNumber": [8]},
+      {"matchCount": 4,"unMatchedNumber": [7,8]},
+      {"matchCount": 3,"unMatchedNumber": [7,8,9]},
+      {"matchCount": 2,"unMatchedNumber": [7,8,9,10]},
+      {"matchCount": 1,"unMatchedNumber": [7,8,9,10,11]},
+      {"matchCount": 0,"unMatchedNumber": [7,8,9,10,11,12]},
+    ]
+    
+    // when
+    const matchResult = await lottoResult.getPurchaseResult();
+
+    // then
+    matchResult.forEach((result, index) => {
+      expect(result.matchCount).toBe(expectedList[index].matchCount);
+      expect(result.unMatchedNumber).toEqual(expectedList[index].unMatchedNumber);
+    });
+  });
+});
---
+++ markdown/test/05_lotto_result_test_list.md
@@ -0,0 +1,6 @@
+# `LottoResult`
+
+## `getPurchaseResult()`
+
+- 구매기록의 당첨 목록 생성
+
