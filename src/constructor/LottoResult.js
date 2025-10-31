71900e3d61eb71f0eb4987559212e8b7ef86d044
thumbthing <ilscc0902@gmail.com>
2025-11-01 03:44:51 +0900

    feat(LottoResult-updateStatus): 당첨 기록 최신화 기능 추가, 유틸 함수 위치 수정
    구현 기능
    - declareStatusKey: status 의 키 매칭
    - updateStatus: 최신화활 status의 값 최신화
    - updateStatusByMatchList: 당첨 목록을 순회하여 status 최신화
    - getUpdatedResultStatus: 비동기 순회 callback, 매개변수 설정후 최신화-결과 반환
    
    위치 수정
    - getPromiseAllResult

--- src/constructor/LottoResult.js
+++ src/constructor/LottoResult.js
@@ -23,7 +23,13 @@ export default class LottoResult {
     return { matchCount, unMatchedNumber }
   }
 
-  // 구매 목록 => 당첨 기록 변환
+  // 비동기로 실행할 순회 기능 처리
+  async #getPromiseAllResult(callbackName, parameter) {
+    const promise = await Promise.all(await this[callbackName](parameter));
+    return promise;
+  }
+
+  // 구매 목록 => 당첨 목록 변환
   createMatchList() {
     const matchList = this.purchaseList.map((purchase) => {
       return this.#getSingleMatchResult(purchase);
@@ -31,24 +37,47 @@ export default class LottoResult {
     return matchList;
   }
 
-  // 비동기로 실행할 순회 기능 처리
-  async #getPromiseAllResult(callbackName) {
-    const promise = await Promise.all(this[callbackName]());
-    return promise;
-  }
 
-  // 당첨 기록 목록 생성
+  // 당첨 목록 생성
   async getMatchList() {
     const matchList = await this.#getPromiseAllResult("createMatchList");
     return matchList;
   }
 
-  // 기록할 필요 없는 당첨 기록 필터링
+  // 기록할 필요 없는 당첨 목록 필터링
   async filterMatchList(matchList) {
     const filterdList = matchList.filter((match) => {
       return match.matchCount >= 3;
     });
     return filterdList;
   }
-}
 
+  // 당첨 기록의 key를 생성
+  declareStatusKey(match) {
+    let statusKey = match.matchCount;
+    if (match.matchCount === 5 && match.unMatchedNumber[0] === this.bonusNumber) statusKey += 1;
+    if (match.matchCount === 6) statusKey += 1;
+    return statusKey;
+  }
+
+  // 생성된 key로 당첨 결과 최신화
+  updateStatus(statusKey) {
+    const currentResultSum = this.status.get(statusKey);
+    this.status.set(statusKey, currentResultSum + 1);
+  }
+
+  // 필터링 된 당첨 기록을 순회
+  async updateStatusByMatchList(matchList) {
+    matchList.forEach((match) => {
+      const statusKey = this.declareStatusKey(match);
+      this.updateStatus(statusKey);
+    });
+    return this.status;
+  }
+
+  // 당첨 목록으로 당첨 기록 최신화
+  async getUpdatedResultStatus(matchList) {
+    const matchStatus = await this.#getPromiseAllResult("updateStatusByMatchList", matchList);
+    return new Map(matchStatus);
+  }
+}
