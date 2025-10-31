ec3a84dcd64d93038e8fa6f6d2bb4f9b4aa76c89
thumbthing <ilscc0902@gmail.com>
2025-10-31 21:07:42 +0900

    style(ValidateRawString): 함수명 구체화

--- src/validate/ValidateRawString.js
+++ src/validate/ValidateRawString.js
@@ -1,11 +1,11 @@
 export default class ValidateRawString {
-  static checkPurchase(purchaseInput) {
+  static checkPurchaseInput(purchaseInput) {
     const purchaseRegExp = /^\d+$/g;
     const isValid = purchaseRegExp.test(purchaseInput);
     if (!isValid) throw new Error('[ERROR] 입력값이 숫자 형식이 아닙니다. 다시 입력해주세요');
   }
 
-  static checkWinNumber(winNumberInput) {
+  static checkWinNumberInput(winNumberInput) {
     const pattern = '(?:\\d{0,1})(?:\\d{1,1})';
     const patternList = `^${Array(6).fill(pattern).join(',')}$`
     const winNumberRegExp = new RegExp(patternList);
@@ -13,7 +13,7 @@ export default class ValidateRawString {
     if (!isValid) throw new Error('[ERROR] 입력값이 유효하지 않습니다.');
   }
 
-  static checkBonusNumber(bonusNumberInput) {
+  static checkBonusNumberInput(bonusNumberInput) {
     const bonusNumberRegExp = /^(?:\d{0,1})(?:\d{1,1})$/g;
     const isValid = bonusNumberRegExp.test(bonusNumberInput);
     if (!isValid) throw new Error('[ERROR] 입력값이 로또 번호 형식이 아닙니다. 다시 입력해주세요');
